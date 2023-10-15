d83428334e80b9515ff3d4f1c83ca3608f18349b
Timo Sandmann <mail@timosandmann.de>
2023-10-15 15:40:13 +0200

    Use Debian 12 for Eclipse 2023-09 containers

--- build_eclipse.sh
+++ build_eclipse.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # ctbot-container
-# Copyright (C) 2022 Timo Sandmann
+# Copyright (C) 2023 Timo Sandmann
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -16,7 +16,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-VERSION=${1:-2022-03}
+VERSION=${1:-2023-09}
 USER_ARGS="${@:2}"
 
 COMMAND=podman
